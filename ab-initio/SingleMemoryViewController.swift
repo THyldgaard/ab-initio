cbc7f226923861fc6d7ccf5bd69785ded187a76e
THyldgaard <t.hyldgaard92@gmail.com>
2016-04-13 22:52:09 +0200

    Created the layout for the show single memory view.

---
+++ ab-initio/SingleMemoryViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SingleMemoryViewController.swift
+//  ab-initio
+//
+//  Created by Tonni Hyldgaard on 4/13/16.
+//  Copyright © 2016 Tonni Hyldgaard. All rights reserved.
+//
+
+import UIKit
+
+class SingleMemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillDisappear(animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBarHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
