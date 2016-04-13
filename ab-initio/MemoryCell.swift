f4d7e7679d40bb4b6f69dcc14558d938fa093b52
THyldgaard <t.hyldgaard92@gmail.com>
2016-04-13 18:36:39 +0200

    Created the tableview with its cells and dummy data. Created tabel cells class. And made the Add button be on top of the tableview allowing the cells to go under it

---
+++ ab-initio/MemoryCell.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryCell.swift
+//  ab-initio
+//
+//  Created by Tonni Hyldgaard on 4/13/16.
+//  Copyright © 2016 Tonni Hyldgaard. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UITableViewCell {
+
+    @IBOutlet weak var mainMemoryImg: UIImageView!
+    @IBOutlet weak var mainMemoryCellText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(image: UIImage, memoryCellText: String) {
+        mainMemoryImg.image = image
+        mainMemoryCellText.text = memoryCellText
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
--- ab-initio/ViewController.swift
+++ ab-initio/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var countriesForMockImages = ["florence", "louvre-france", "copenhagen-denmark"]
+    var countriesForMockLabel = ["Italy", "France", "Denmark"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCellWithIdentifier("MemoryCell") as? MemoryCell {
+            
+            let img = UIImage(named: countriesForMockImages[indexPath.row])!
+            
+            cell.configureCell(img, memoryCellText: countriesForMockLabel[indexPath.row])
+            
+            return cell
+        } else {
+            return MemoryCell()
+        }
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
 
